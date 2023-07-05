947b1d2ab34f7bc11cdf8a29afb83a5102050b33
Arthur BERTAUD <arthur.bertaud44@gmail.com>
2023-07-05 23:55:02 +0200

    Time: 55 ms (80.46%), Space: 16.6 MB (35.82%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,25 @@
+class Solution:
+        def isValid(self, c, d) -> bool:
+            return c == d or d == '.'
+        
+        def isMatch(self, s: str, p: str) -> bool:
+            memo = {}
+    
+            def dp(i: int, j: int) -> bool:
+                if (i, j) in memo:
+                    return memo[(i, j)]
+            
+                if j == len(p):
+                    return i == len(s)
+            
+                first_match = i < len(s) and self.isValid(s[i], p[j])
+            
+                if j + 1 < len(p) and p[j+1] == '*':
+                    ans = dp(i, j+2) or (first_match and dp(i+1, j))
+                else:
+                    ans = first_match and dp(i+1, j+1)
+            
+                memo[(i, j)] = ans
+                return ans
+        
+            return dp(0, 0)
